733995d5edd8af3840a02dc5f600ed20b22c9323
muhammadasifdotai <sethrolin2573@gmail.com>
2024-08-10 23:07:30 +0500

    svg setup completed

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,3 @@
+export const SVG = {
+    MaleIcon: require('./MaleIcon.svg').default
+}
--- src/routes/TabNavigation.tsx
+++ src/routes/TabNavigation.tsx
@@ -2,14 +2,19 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import ProfileScreen from "../screens/ProfileScreen";
 import HomeScreen from "../screens/HomeScreen";
+import { SVG } from "../assets";
 
 const Tab = createBottomTabNavigator()
 
 export default function TabNavigation(): JSX.Element {
     return (
             <Tab.Navigator screenOptions={{headerShown: false, tabBarActiveTintColor: 'red', tabBarInactiveTintColor: 'black'}}>
-                <Tab.Screen name="HomeScreen" component={HomeScreen}/>
-                <Tab.Screen name="ProfileScreen" component={ProfileScreen}/>
+                <Tab.Screen name="HomeScreen" component={HomeScreen} options={{
+                    tabBarIcon: ({color}) =>  <SVG.MaleIcon color={color}/>
+                }}/>
+                <Tab.Screen name="ProfileScreen" component={ProfileScreen} options={{
+                    tabBarIcon: ({color}) => <SVG.MaleIcon color={color} />
+                }}/>
             </Tab.Navigator>
     )
 }
